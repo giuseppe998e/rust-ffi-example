e5f96affc0c3a6965056b9f1593e48b3a1bb4595
Giuseppe Eletto <peppe.eletto@gmail.com>
2021-04-29 21:26:52 +0200

    Fix typos in example.c

--- example.c
+++ example.c
@@ -7,9 +7,9 @@ int main(int argc, char *argv[]) {
       rsub = rust_sub(x, y),
       rmul = rust_mul(x, y),
       rdiv = rust_div(x, y);
-      
+
   printf("Rust fn sum(x,y) -> z: %d + %d = %d\n", x, y, rsum);
-  printf("Rust fn sub(x,y) -> z: %d + %d = %d\n", x, y, rsub);
-  printf("Rust fn mul(x,y) -> z: %d + %d = %d\n", x, y, rmul);
-  printf("Rust fn div(x,y) -> z: %d + %d = %d\n", x, y, rdiv);
+  printf("Rust fn sub(x,y) -> z: %d - %d = %d\n", x, y, rsub);
+  printf("Rust fn mul(x,y) -> z: %d * %d = %d\n", x, y, rmul);
+  printf("Rust fn div(x,y) -> z: %d / %d = %d\n", x, y, rdiv);
 }
